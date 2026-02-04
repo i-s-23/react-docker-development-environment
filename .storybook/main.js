53dff002365f07a01b1e69fbed6bd71a4cff9a67
is23 <sumosumokingin@gmail.com>
2026-02-04 04:03:32 +0000

    バージョンアップに伴う各種修正

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,17 @@
-module.exports = {
-  stories: ['../src/**/*.stories.js'],
+/** @type { import('@storybook/react-vite').StorybookConfig } */
+const config = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
-    '@storybook/preset-create-react-app',
-    '@storybook/addon-actions',
-    '@storybook/addon-links',
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
   ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
 };
+export default config;
